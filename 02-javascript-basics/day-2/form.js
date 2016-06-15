eb61db0e7adc87862c7f4dcd4423ad8b76fea36a
Orlando Caraballo <orlando@nycda.com>
2016-06-15 14:29:06 -0400

    more comments

--- 02-javascript-basics/day-2/form.js
+++ 02-javascript-basics/day-2/form.js
@@ -1,7 +1,11 @@
+// first we get the element we need to work with (getElementById)
+//  then we associate the behavior we want by setting onclick = [an
+//  anonymous function]
 document.getElementById("submit-button").onclick = function(event) {
   event.preventDefault()
 
-  console.log("sdfsd")
+  // when we click on the submit-button it logs the text 
+  console.log("I am logging like a logger")
 }
 
 document.getElementById("name").onkeypress = function(event) {
