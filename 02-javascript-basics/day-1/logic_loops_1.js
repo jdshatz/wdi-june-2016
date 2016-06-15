4c8e25c1f04cb4afe6af6358a00a2678f95e4111
Orlando Caraballo <orlando@nycda.com>
2016-06-15 14:39:30 -0400

    comments again

--- 02-javascript-basics/day-1/logic_loops_1.js
+++ 02-javascript-basics/day-1/logic_loops_1.js
@@ -1,13 +1,18 @@
 var number = 5
 
+// here we check to see if number is less than 10
+//  if so then log "less than 10"
 if(number < 10) {
   console.log("less than 10")
 } else {
+  // whenever you have a else keyword you must have a corresponding if
   console.log("the var value is " + number + " and is greater than 10")
 }
 
 var str = "is this the same?"
 
-if(number == "is this the same") {
+// we check to see if "str" matches the value "is this the same"
+if(str == "is this the same") {
+  // in this case it doesn't because its missing a question mark
   console.log("this is the same")
 }
--- 02-javascript-basics/day-1/logic_loops_2.js
+++ 02-javascript-basics/day-1/logic_loops_2.js
@@ -1,15 +1,23 @@
+// this defines a function you can use later on in the code
 function logit(text) {
   console.log(text)
 }
 
+// this invokes the function we created above
 logit("I am logging this friends")
 
+
+// this defines a function but with no arguments
 function logitNoArguments() {
   console.log("I am logging this with no arguments")
 }
 
+// here we invoke the function we defined above
 logitNoArguments()
 
+// we define a function named "chooseDoor" where
+//  depending on which door number we pass in as an argument
+//  indicated by the argument "doorNum" we log different results into the console
 function chooseDoor(doorNum) {
   if(doorNum == 1) {
     console.log("you won a car")
@@ -22,4 +30,5 @@ function chooseDoor(doorNum) {
   }
 }
 
+// here we invoke the "chooseDoor" function and pass it the value 1
 chooseDoor(1)
