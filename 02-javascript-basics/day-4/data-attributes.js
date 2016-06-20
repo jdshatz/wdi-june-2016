11965720d0a0a3da771b6335f3d73ed4c62428d7
Orlando Caraballo <orlando@nycda.com>
2016-06-20 14:33:04 -0400

    added more comments

--- 02-javascript-basics/day-4/data-attributes.js
+++ 02-javascript-basics/day-4/data-attributes.js
@@ -1,16 +1,27 @@
-$(document).ready(function(){
-  $("a").data("foo", "dskjhksdlj")
+// ensures we only run this code once
+//  the entire html document is loaded
+$(document).ready(function() {
 
+  // sets an event handler that responds to clicking an
+  //  anchor
   $("a").click(function(event){
+    // gets the current element that was clicked
+    //  then gets the value of the attribute "img-url"
     var imgUrl = $(event.target).data("img-url")
 
-    $("body").append("<div class='overlay'><img src='" + imgUrl + "'></overlay>")
+    // appends a new img element wrapped in a div to the body
+    //  notice the src of the img is set to the whatever
+    //  imgUrl is set equal to
+    $("body").append("<div class='overlay'><img src='" + imgUrl + "'></div>")
   })
 
-  $("body").on("click", "img", function(){
-    $(this).hide()
-    $(this).closest(".overlay").hide()
-  })
+  // this adds a new click handler
+  $("body").on("click", "img", function(event){
+    // hides the element clicked
+    $(event.target).hide()
 
-  console.log($("a").data("img-url"))
+    // finds the closest element with the class .overlay
+    //  and hides that too
+    $(event.target).closest(".overlay").hide()
+  })
 })
